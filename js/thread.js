6928c6572172f76607b7d022c30b0a3425aa7347
Orest oliinyk <orest.macsanta@gmail.com>
2016-03-29 23:29:59 +0300

    Event Subscribe Model

--- js/thread.js
+++ js/thread.js
@@ -1,10 +1,13 @@
-var thread = function thread(){
+var thread = function thread(){};
 
-
-};
-thread.getEvent = function()
+thread.getEvent = function(event)
 {
-
+  var event_data = JSON.parse(event.data);
+  if(event_data.thread_id==thread.ejsThread.t_id){
+    thread.ejsThread.onEvent(event_data, thread.ejsThread.first());
+  }else{
+    console.log('Thread is not openned!');
+  }
 }
 
 window.EventModules['thread'] = thread;
